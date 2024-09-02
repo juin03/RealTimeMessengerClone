d57fe0896729ae95e6bf8eee3548200fad829563
juin03 <juin5789@student.usm.my>
2024-09-02 21:24:06 +0800

    implemented MessageController

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class MessageResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +15,17 @@ class MessageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'message' => $this->message,
+            'sender_id' => $this->sender->id,
+            'receiver_id' => $this->receiver->id,
+            'sender' => new UserResource($this->sender), // User object from the UserResource.php
+            'group_id' => $this->group_id,
+            'attachments' => MessageAttachmentResource::collection($this->attachments),
+            'created_at' => $this->created_at,
+            'update_at' => $this->update_at,
+
+        ];
     }
 }
